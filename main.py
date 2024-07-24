206c15d1cbe0bcaec8efc09e16048e7d4b41772a
realTB <34286253+realTB@users.noreply.github.com>
2024-07-24 02:00:57 -0400

    Disable CORS checks

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI;
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from Endpoints.Endpoints import router
 InfoGrepFileManagementService = FastAPI();
 
 InfoGrepFileManagementService.include_router(router)
+InfoGrepChatroomService.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"], allow_credentials=True)
 if __name__ == "__main__":
     uvicorn.run(InfoGrepFileManagementService, host="0.0.0.0", port=8002)
