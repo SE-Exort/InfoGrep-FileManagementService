2e18427653169848ccc34ffd1f8554935f0970f4
Tanbir Singh Banipal <34286253+realTB@users.noreply.github.com>
2025-02-16 21:33:41 -0500

    Move File Storage code into FileSystem Backend

---
+++ Backends/backend.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+from fastapi import UploadFile
+
+class Backend(ABC):
+    def __init__(self, location):
+        self.location = location;
+        return;
+
+    @abstractmethod
+    async def save_file(self, file_uuid: str, uploadedfile: UploadFile):
+        pass;
+
+    @abstractmethod
+    def delete_file(self, file_uuid: str):
+        pass;
+
+    @abstractmethod
+    def get_file(self, file_uuid: str, file_name: str):
+        pass;
