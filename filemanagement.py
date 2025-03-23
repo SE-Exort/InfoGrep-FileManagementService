a253ce5e95e1992b2b4af88124570603094418df
TyroneHe-0926 <tyrone99926@gmail.com>
2025-03-22 21:23:56 -0400

    fix: adding connection timeout handler

--- filemanagement.py
+++ filemanagement.py
@@ -7,7 +7,22 @@ class filemanagement:
     def __init__(self):
         # init logger
         self.logger = Logger("FileManagementServiceLogger")
-
+        self.initDbConnection()
+        self.cursor = self.con.cursor()
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS filelists (\
+                                FILEUUID CHAR(36) PRIMARY KEY,\
+                                CHATROOM CHAR(36) NOT NULL,\
+                                FILENAME VARCHAR NOT NULL,\
+                                UPLOADUSER CHAR(36) NOT NULL,\
+                                FILESIZE BIGINT NOT NULL)")
+        
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS filebackend (\
+                            FILEUUID CHAR(36) PRIMARY KEY,\
+                            FILEOID INT NOT NULL\
+                            )")
+        self.cursor.close();
+    
+    def initDbConnection(self):
         # DB config
         db_port = "5432"
         db_host = os.environ.get("PGHOST", "localhost")
@@ -40,41 +55,42 @@ class filemanagement:
             self.con = psycopg2.connect(database=db_name, user=db_user, password=db_password, host=db_host, port=db_port, **keepalive_kwargs)
             self.logger.info("DB connection established")
         self.con.autocommit = False;
-        self.cursor = self.con.cursor()
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS filelists (\
-                                FILEUUID CHAR(36) PRIMARY KEY,\
-                                CHATROOM CHAR(36) NOT NULL,\
-                                FILENAME VARCHAR NOT NULL,\
-                                UPLOADUSER CHAR(36) NOT NULL,\
-                                FILESIZE BIGINT NOT NULL)")
-        
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS filebackend (\
-                            FILEUUID CHAR(36) PRIMARY KEY,\
-                            FILEOID INT NOT NULL\
-                            )")
-        self.cursor.close();
+
+    def getConnection(self):
+        try:
+            cursor = self.con.cursor()
+            cursor.close()
+            return self.con
+        except Exception:
+            self.logger.warning("Pyscopg2 connection closed, re-establishing ...")
+            self.initDbConnection()
+            return self.con
         
     def getFilesFromChatroom(self, chatroom_uuid):
-        cursor = self.con.cursor();
+        connection = self.getConnection()
+        cursor = connection.cursor();
         cursor.execute("SELECT FILEUUID, FILENAME FROM filelists WHERE CHATROOM = %s", (str(chatroom_uuid),))
         result = cursor.fetchall();
         cursor.close();
         return result;
 
     def createFile(self, user_id, chatroom_uuid, file_uuid, file_name, file_size):
-        cursor = self.con.cursor();
+        connection = self.getConnection()
+        cursor = connection.cursor();
         cursor.execute("INSERT INTO filelists(FILEUUID,CHATROOM,FILENAME,UPLOADUSER,FILESIZE) VALUES(%s,%s,%s,%s,%s)", (str(file_uuid),str(chatroom_uuid),str(file_name),str(user_id),file_size))
-        self.con.commit()
+        connection.commit()
         cursor.close();
     
     def deleteFile(self, chatroom_uuid, file_uuid):
-        cursor = self.con.cursor();
+        connection = self.getConnection()
+        cursor = connection.cursor();
         cursor.execute("DELETE FROM filelists WHERE CHATROOM = %s AND FILEUUID = %s", (str(chatroom_uuid), str(file_uuid)))
-        self.con.commit()
+        connection.commit()
         cursor.close();
 
     def isValidFile(self, chatroom_uuid, file_uuid):
-        cursor = self.con.cursor();
+        connection = self.getConnection()
+        cursor = connection.cursor();
         cursor.execute("SELECT * FROM filelists WHERE CHATROOM = %s AND FILEUUID = %s", (str(chatroom_uuid), str(file_uuid)))
         fileexists = cursor.fetchall()
         cursor.close();
@@ -83,27 +99,31 @@ class filemanagement:
         return 1
 
     def getFileName(self, chatroom_uuid, file_uuid):
-        cursor = self.con.cursor();
+        connection = self.getConnection()
+        cursor = connection.cursor();
         cursor.execute("SELECT FILENAME FROM filelists WHERE CHATROOM = %s AND FILEUUID = %s", (str(chatroom_uuid), str(file_uuid)))
         matchingfiles = cursor.fetchone()
         cursor.close();
         return matchingfiles[0]
 
     def adminGetAllFiles(self):
-        cursor = self.con.cursor();
+        connection = self.getConnection()
+        cursor = connection.cursor();
         cursor.execute("SELECT FILEUUID, CHATROOM, UPLOADUSER, FILENAME, FILESIZE FROM filelists")
         result =  cursor.fetchall()
         cursor.close();
         return result;
 
     def adminDeleteFile(self, file_uuid):
-        cursor = self.con.cursor();
+        connection = self.getConnection()
+        cursor = connection.cursor();
         cursor.execute("DELETE FROM filelists WHERE FILEUUID = %s", (str(file_uuid),))
-        self.con.commit()
+        connection.commit()
         cursor.close();
 
     def adminIsValidFile(self, file_uuid):
-        cursor = self.con.cursor();
+        connection = self.getConnection()
+        cursor = connection.cursor();
         cursor.execute("SELECT * FROM filelists WHERE FILEUUID = %s", (str(file_uuid),))
         fileexists = cursor.fetchall()
         cursor.close();
@@ -112,33 +132,36 @@ class filemanagement:
         return 1
 
     def backendSaveFile(self, file_uuid, file):
-        write_object = self.con.lobject(0, 'w');
+        connection = self.getConnection()
+        write_object = connection.lobject(0, 'w');
         write_object.write(file)
-        self.con.commit()
-        cursor = self.con.cursor();
+        connection.commit()
+        cursor = connection.cursor();
         cursor.execute("INSERT INTO filebackend(FILEUUID,FILEOID) VALUES(%s,%s)", (str(file_uuid),str(write_object.oid),))
-        self.con.commit()
+        connection.commit()
         cursor.close();
         return
     
     def backendReadFile(self, file_uuid):
-        cursor = self.con.cursor();
+        connection = self.getConnection()
+        cursor = connection.cursor();
         cursor.execute("SELECT FILEOID FROM filebackend WHERE FILEUUID = %s", (str(file_uuid),));
         file_oid = cursor.fetchone()[0]
-        result = self.con.lobject(file_oid, 'b');
+        result = connection.lobject(file_oid, 'b');
         cursor.close();
         return result
 
     def backendDeleteFile(self, file_uuid):
         #delete the actual file
-        cursor = self.con.cursor();
+        connection = self.getConnection()
+        cursor = connection.cursor();
         cursor.execute("SELECT FILEOID FROM filebackend WHERE FILEUUID = %s", (str(file_uuid),));
         file_oid = cursor.fetchone()[0]
-        file = self.con.lobject(file_oid, 'r');
+        file = connection.lobject(file_oid, 'r');
         file.unlink();
-        self.con.commit();
+        connection.commit();
 
         #delete the reference to the file
         cursor.execute("DELETE FROM filebackend WHERE FILEUUID = %s", (str(file_uuid), ));
-        self.con.commit()
+        connection.commit()
         cursor.close();
